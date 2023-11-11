537ca028a4f0f56bdfe76599938cf400c8f04a76
Festus <festusyuma@gmail.com>
2023-11-11 22:35:30 +0100

    remove auth guard against health endpoint

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,20 +1,10 @@
 import { CustomRes } from '@backend-template/http';
-import { Controller, Get, UseGuards } from '@nestjs/common';
-
-import { AppService } from './app.service';
-import {
-  Authenticated,
-  AuthenticatedGuard,
-} from '@backend-template/rest-server';
-import { UserData } from '@backend-template/types';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
-@UseGuards(AuthenticatedGuard)
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get('health')
-  async getData(@Authenticated() user: UserData) {
-    return CustomRes.success(user);
+  async getData() {
+    return CustomRes.success();
   }
 }
